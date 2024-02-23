f225cf5df5f397a832dd6b4ab4ee6e9c6cf7107e
gyl <48873542+gyl30@users.noreply.github.com>
2024-02-23 17:21:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ gcc --version
 g++ --version
 cmake --version
 
-wget -c https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.0/llvm-project-15.0.0.src.tar.xz
-tar xf llvm-project-15.0.0.src.tar.xz
-cd llvm-project-15.0.0.src && mkdir build && cd build
+wget -c https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz
+tar xf llvm-project-17.0.6.src.tar.xz
+cd llvm-project-17.0.6.src && mkdir build && cd build
 scl enable devtoolset-11 bash
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/llvm -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -G "Unix Makefiles" ../llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -G "Unix Makefiles" ../llvm
 make -j
+echo "------------------------------------------------------------------"
+ldd $HOME/llvm/bin/clang
+echo "------------------------------------------------------------------"
+ldd $HOME/llvm/bin/clangd
