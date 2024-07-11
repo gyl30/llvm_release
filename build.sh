ad8a84d4f66cd61fef62dd3cd466ac1ec20a4737
gyl <48873542+gyl30@users.noreply.github.com>
2024-07-11 13:20:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+echo "------------------------------------------------------------------"
 source /opt/rh/devtoolset-11/enable
 gcc --version
 g++ --version
 cmake --version
+echo "------------------------------------------------------------------"
 
-wget -c https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz
-tar xf llvm-project-17.0.6.src.tar.xz
-cd llvm-project-17.0.6.src && mkdir build && cd build
-source /opt/rh/devtoolset-11/enable
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -G "Unix Makefiles" ../llvm
-make -j
+git clone https://github.com/llvm/llvm-project
+cd llvm-project
+
+
+GIT_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "------------------------------------------------------------------"
-ldd $HOME/llvm/bin/clang
+echo "git checkout ${GIT_TAG}"
 echo "------------------------------------------------------------------"
-ldd $HOME/llvm/bin/clangd
+git checkout ${GIT_TAG}
+
+mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/llvm -DLLVM_ENABLE_PROJECTS="clang;lld;lldb;clang-tools-extra" LLVM_ENABLE_RUNTIMES=all -G "Unix Makefiles" ../llvm 
+
+make -j`nproc` && make install && cd .. && rm -rf build
